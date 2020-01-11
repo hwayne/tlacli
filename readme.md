7725cb27ec72e239e61b76d075f427206c71a1f1
Hillel <hwayne@gmail.com>
2020-01-10 22:27:54 -0600

    I forgot to stage the new changes

--- readme.md
+++ readme.md
@@ -1,3 +1,106 @@
 # tlacli: A CLI tool for TLA+
 
-**DISCLAIMER:** This is not a 
+**DISCLAIMER:** This is not an official TLA+ tool and isn't a prototype for one. I'm not making any guarantees of backwards compatibility or non-breaking changes or whatever. It's just a script I find useful.
+
+`tlacli` is a tool for running the TLC model checker from the command line. You can already run TLC from the command line, anyway, using `tlc2.TLC`, and `tlacli` only provides a subset of the functionality. It still has a few UX improvements, though:
+
+1. Nicer flag UX. Arguments follow the conventional "flag" format. You can spot-check a spec with just `python tlacli.py specfile.tla`. 
+1. Saner defaults. It automatically uses `Spec` as your temporal formula, [TODO defaults to using a worker per CPU core], cleans up the `states` directory,
+1. You don't have to write a config file. You can define invariants, properties, and constants as command-line flags and `tlacli` will automatically build the proper config file for that run. You can also save the configuration as a template for future runs. You can also use _both_ a config file and flags, where the config is a template and the flags are specializations.
+
+## Setup
+
+You need Java and Python 3.8. There's no package, just clone and run with `python tlacli.py`.
+
+## Using
+
+**You need to be in the directory with the spec**. IIRC there's something in the tla+ issue tracker about this, but I haven't searched yet. Run with
+
+```
+python tlacli.py specfile.tla # no ./ first
+```
+
+By default, this runs `specfile.tla` with the specification `Spec`. You can change the run specification with the `--spec` flag. By default, this runs TLC with the `-terse` and `-cleanup` flags. The config file will be saved as `temporary.cfg`. You can change the filename with `--cfg-out {name}`.
+
+**NOTE:** Running currently creates an empty `states` directory.
+
+### Properties
+
+You can specify invariants and properties from the command line. Use the `--invariant {inv}` flag and the `--property {prop}` flag, respectively. Both accept multiple operators.
+
+**NOTE:** If `--invariant` or `--property` are the _last_ flags passed in, the script will assume your specfile is an invariant! You can prevent this by adding a `--`.
+
+```
+python tlacli.py --invariant Inv1 Inv2 -- specfile.tla
+```
+
+You can also use `--inv` and `--prop`, but this may change in the future.
+
+### Constants
+
+You can assign constants with `--constant {name} {value}`. Each constant must be a separate flag. You can put in sets, tuples, etc by putting `{value}` in quotes. Use single quotes if you want to put in strings.
+
+```
+python tlacli.py --constant Max 4 --constant Threads '{1, 2}' specfile.tla
+python tlacli.py --constant Colors '{\"red\", \"green\"}' specfile.tla
+
+```
+
+#### Model Values
+
+If you need several model values, you can specify them all in a single `--model-values {m1} {m2} ...` flag.
+
+```
+python tlacli.py --model_values A B C Null Server -- specfile.tla
+```
+
+#### Sets of Model Values
+
+Use an ordinary assignment. You don't need a `--model-values` flag first.
+
+```
+# Wrong
+python tlacli.py --model-values m1 m2 m3 --constant ModelSet "{m1, m2, m3}" specfile.tla
+
+
+# Right
+python tlacli.py --constant ModelSet "{m1, m2, m3}" specfile.tla
+```
+
+Symmetry sets are not yet supported.
+
+### Configuration Templates
+
+You can specify a template configuration with `--cfg template.cfg`:
+
+```
+python tlacli.py --cfg foo.cfg specfile.tla
+```
+
+`tlacli` can only read things that are also expressible as flags. Currently, this means invariants, properties, specification, and (most) constants. Everything else is ignored. It's a simple text parser and may miss things formated in an unexpected way. The one guarantee: If you write a file a config with `--cfg-out` and later read it with `--cfg`, the whole config will be read properly.
+
+A template can be used in conjunction with the other flags. Currently this adds the additional flags on top of the template. The plan is that if the flags and the template conflict, the flags take priority. This will let us specialize a template.
+
+**BUG:** Constants are currently additive. That doesn't make sense
+
+## Contributing
+
+Eh make a PR or something
+
+## TODO
+
+* Allow overwriting constants when both in template and flags
+* Translating PlusCal (probably means implementing subparsers)
+* Get the "no-inv" flags working
+* Implement and document all the TLC options here: https://lamport.azurewebsites.net/tla/tlc-options.html
+* "Set of Model Values" affordance flag [[unecessary, document this]]
+* Symmetry model sets
+* More post-run cleanup
+* Automatically calculate base number of workers per run
+* Advanced config options:
+    * VIEW (chaos reigns)
+    * Operator Overrides / Constant Operators
+    * CONSTRAINT and ACTION-CONSTRAINT
+    * SYMMETRY
+* Explanations on what you can and can't assign in a config file (anything that doesn't require `EXTENDS`, I think)
+* Writing on landmines and stuff
--- tlacli.py
+++ tlacli.py
@@ -1,13 +1,25 @@
 import argparse
+import os
+import re
 import subprocess
 import sys
-from itertools import chain
 from collections import defaultdict
-import re
+from itertools import chain
+from multiprocessing import cpu_count  # For default number of worker threads
+from pathlib import Path
+from typing import List, TypeVar
+
+T = TypeVar('T')
 
 # TODO, this seems to fail if you pass in a path. I think you need to be in the same directory or something for tlc to work
 
-def extract_cfg(cfg_path):
+def unique(l: List[T]) -> List[T]:
+    return list(set(l))
+
+def flatten(l: List[List[T]]) -> List[T]:
+    return list(chain.from_iterable(l))
+
+def extract_cfg(cfg_path: str):
     """This current follows an EXTREMELY rigid format. As a rule of thumb, if this script writes a cfg and then reads it, it will get an identical configuration. All other behaviors are undefined."""
     out = defaultdict(list) # So we can append
     with open(cfg_path) as f:
@@ -15,25 +27,26 @@ def extract_cfg(cfg_path):
     for line in cfg:
         line = line.strip()
         # Okay, okay, maybe the walrus operator is a good idea.
+        # If I stick with extends for argparse might as well use them, since we're stuck on 3.8 anyway then
 
         # SPECIFICATION
-        match = re.match("SPECIFICATION (\w+)", line)
+        match = re.match(r"SPECIFICATION (\w+)", line)
         if match:
             out["spec"] = match[1]
 
         # INVARIANT
-        match = re.match("INVARIANT (\w+)", line)
+        match = re.match(r"INVARIANT (\w+)", line)
         if match:
             out["invariants"].append(match[1])
 
         # TEMPORAL PROPERTIES
-        match = re.match("PROPERTY (\w+)", line)
+        match = re.match(r"PROPERTY (\w+)", line)
         if match:
             out["properties"].append(match[1])
 
         # CONSTANTS
         # This regex is imperfect for ordinary assignments
-        match = re.match("(\S+)\s?=\s?(.+)", line)
+        match = re.match(r"(\S+)\s?=\s?(.+)", line)
         if match:
             if match[1] == match[2]:
                 out["model_values"].append(match[1])
@@ -60,16 +73,18 @@ def construct_cfg(args=None):
     # We use list(set()) to uniquify the list
     
     cfg_dict["invariants"] += args.invariant
-    cfg_dict["invariants"] = list(set(cfg_dict["invariants"]))
+    cfg_dict["invariants"] = unique(cfg_dict["invariants"])
 
     cfg_dict["properties"] += args.property
-    cfg_dict["properties"] = list(set(cfg_dict["properties"]))
+    cfg_dict["properties"] = unique(cfg_dict["properties"])
 
     cfg_dict["constants"] += args.constant
-    cfg_dict["constants"] = list(set(cfg_dict["constants"]))
+    cfg_dict["constants"] = unique(cfg_dict["constants"])
 
+    # TODO overwrite exising model values from cfg
+    # Maybe model_values should be stored as a dict
     cfg_dict["model_values"] += args.model_values
-    # TODO filter out no-invariants
+    # TODO filter out no-invariants and no-properties
 
     out = [f"SPECIFICATION {cfg_dict['spec']}"]
     for inv in cfg_dict["invariants"]:
@@ -87,7 +102,7 @@ def construct_cfg(args=None):
     # TODO should this use <- instead of = ?
     # Would break the regex for reading in cfgs
     if cfg_dict["constants"]:
-        out.append("CONSTANTS \* regular assignments")
+        out.append('CONSTANTS \* regular assignments')
         for constant, value in cfg_dict["constants"]:
             out.append(f"  {constant} = {value}")
     return "\n".join(out)
@@ -95,38 +110,57 @@ def construct_cfg(args=None):
 parser = argparse.ArgumentParser()
 cfg_args = parser.add_argument_group("cfg_args", "Configuration values for the TLA+ spec")
 tlc_args = parser.add_argument_group("tlc_args", "Runtime values for the TLC model checker")
+# https://lamport.azurewebsites.net/tla/tlc-options.html
+
 cfg_args.add_argument("--spec", "--specification", default="Spec", help="The TLA+ specification operator, defaults to Spec")
 cfg_args.add_argument("--cfg", help="A template cfg for default values")
-cfg_args.add_argument("--invariant", default=[], action="append", nargs='*', help="Adds argument as model invariant, may be specified multiple times")
-cfg_args.add_argument("--no-invariant", default=[], action="append", help="Invariants that should NOT be checked")
-cfg_args.add_argument("--property", default=[], action="append", nargs='*', help="Adds argument as model temporal property, may be specified multiple times")
-cfg_args.add_argument("--no-property", default=[], action="append", help="Temporal Property that should NOT be checked")
 
-cfg_args.add_argument("--constant", default=[], nargs=2, action="append", help="Constants???")
-cfg_args.add_argument("--model-values", default=[], nargs='+', help="Constants???")
+# Extend is python 3.8 only...
+cfg_args.add_argument("--invariant", default=[], action="extend", nargs='*', help="Adds argument as model invariant, may be specified multiple times")
+cfg_args.add_argument("--no-invariant", default=[], action="extend", help="Invariants that should NOT be checked")
+cfg_args.add_argument("--property", default=[], action="extend", nargs='*', help="Adds argument as model temporal property, may be specified multiple times")
+cfg_args.add_argument("--no-property", default=[], action="extend", help="Temporal Property that should NOT be checked")
+
+# This needs to be append so we get them in pairs matching constants to their assignments
+cfg_args.add_argument("--constant", default=[], nargs=2, action="append", help='{name} {value}')
+cfg_args.add_argument("--model-values", default=[], nargs='+', help='list of model values')
 
 parser.add_argument("Specfile", help="The specfile.tla")
+parser.add_argument("--cfg-out", default="temporary.cfg", help="Where to save the cfg file, if you want to reuse it")
+# TODO parser.add_argument("--cfg-del", action="store_true", help="If added, deletes cfg file after model checking is complete")
+# TODO for tlc arguments, use store_true and store_false. Might also want to move the parser config into a separate file because there are a lot
+
+# Maybe the default should be half the threads?
+tlc_args.add_argument("--tlc-workers", default=cpu_count(), help="The number of worker threads to use (default is number of cpus)")
+tlc_args.add_argument("--tlc-tool", action="store_true", help="If true, outputs debug information")
+
+# TODO --tlc-no-defaults
+# TODO automatic TLC passthrough option. Would disable all the other tlc arguments except config
+
 args = parser.parse_args()
 
-# Flatten invariants plz
-args.invariant = list(chain.from_iterable(args.invariant))
-args.property = list(chain.from_iterable(args.property))
-
 # We need constants to be a list of tuples for flattening purposes
+# TODO make it a dict
 args.constant = [(x, y) for x, y in args.constant]
 
 
 cfg = construct_cfg(args=args)
 # We don't use the temporary module because it closes the file when we're done, and we need to pass a filepath into tlc.
-cfg_file = "temporary.cfg"
+cfg_file = args.cfg_out
 
 print(cfg)
 with open(cfg_file, 'w') as f:
     f.write(cfg)
-#tla2tools.jar
-# TODO os.path
-script = f"java -jar {sys.path[0]}\\tla2tools.jar -workers 6 -config {cfg_file} -terse -cleanup {args.Specfile}"
+
+# Actually run stuff
+
+
+
+jar_path = Path(sys.path[0], "tla2tools.jar")
+script = f"java -jar {jar_path} -workers {args.tlc_workers} -config {cfg_file} -terse -cleanup {args.Specfile}"
+print(script)
 result = subprocess.call(script, shell=True)
 
 sys.exit(result)
-# DOes this seriously create an empty folder even when we cleanup the states?!
+# Does this create an empty folder even when we cleanup the states?
+# TODO remove temporary if flag enabled
